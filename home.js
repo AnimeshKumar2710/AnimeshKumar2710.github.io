7440e430d4983bfa01ded8487940722eb7527e69
AnimeshKumar2710 <74147077+AnimeshKumar2710@users.noreply.github.com>
2021-06-14 18:58:06 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,15 @@
+var pngCats = ["cat", "kitty"];
+var randomNumberOne = Math.floor(Math.random() * 2);
+
+if ($(document).width() < 1000){
+    $(".top").hide();
+    $("." + pngCats[randomNumberOne]).show();
+    if(randomNumberOne == 0){
+        $(".coding").show().addClass("coding-update").removeClass("coding");
+    }else{
+        $(".coding").show().removeClass("coding");
+    }
+    $("." + pngCats[randomNumberOne]).addClass("mobile-img").removeClass(pngCats[randomNumberOne]);
+}else{
+    $(".top").show();
+}
