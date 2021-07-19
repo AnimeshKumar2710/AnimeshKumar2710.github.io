86a07bce64d387cf39049d3703d301fa3c89d6f7
AnimeshKumar2710 <74147077+AnimeshKumar2710@users.noreply.github.com>
2021-07-19 20:55:48 +0530

    Update script.js

--- projects/pokemon/script.js
+++ projects/pokemon/script.js
@@ -1,26 +1,26 @@
-const random = Math.floor(Math.random() * 152);
-const url = "https://pokeapi.co/api/v2/pokemon/" + random;
-console.log(url);
+const random = Math.floor(Math.random() * 152); //getting a random pokemon from first gen
+const url = "https://pokeapi.co/api/v2/pokemon/" + random; // creating the url for the request
+//console.log(url);
 
-async function game(){
+async function game(){  //waiting for the call, async because using fetch
     
-    const response = await fetch(url);
-    const res = await response.json();
-    console.log(res);
+    const response = await fetch(url); //await to wait for the response
+    const res = await response.json(); //await to get the json response
+    //console.log(res);
 
     
-    $(".output").addClass("hidden").removeClass("poke");
+    $(".output").addClass("hidden").removeClass("poke");    //swaping the classes for animation 
     setTimeout(function(){
-        $(".output").attr("src", "flash.png").removeClass("hidden");
+        $(".output").attr("src", "flash.png").removeClass("hidden"); //changing the image to flash
         setTimeout(function(){
             $(".output").addClass("hidden");
-        }, 50);
+        }, 50); //setting timeout inside timeout because the way it gets ignored outside
     }, 1000);
     
    setTimeout(function(){
-    $(".output").attr("src", res.sprites.versions['generation-v']['black-white'].animated.front_default);
-    $(".output").removeClass("hidden");
-    $(".name").text(res.name);
+    $(".output").attr("src", res.sprites.versions['generation-v']['black-white'].animated.front_default); //changing the source to the animation from the api response
+    $(".output").removeClass("hidden"); //the image is still hidden so unhide it
+    $(".name").text(res.name); // adding the text
    }, 2000);
 
 }
